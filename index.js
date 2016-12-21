f567361ea5f17d8c5281aebc3012fcf82556d717
Jishnu A P <Jishnu.Ap@gmail.com>
2016-12-21 22:25:53 +0530

    Installed typings for express and node

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello Worlds!');
 });
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
     console.log('Example app listening on port 3000!');
 });
 //# sourceMappingURL=index.js.map
